0fd4e83dc770f5067caad52d02812b18414825f1
Gnanaraj2006 <krishdeveloper2006@gmail.com>
2025-07-14 08:01:24 +0530

    Saturday work in internship

---
+++ cls_obj.java
@@ -0,0 +1,21 @@
+class hotel{
+    String dosa = "10";
+    String idli = "20";
+    }
+
+public class cls_obj{
+    public static void main(String args[]){
+
+        hotel server = new hotel();
+        System.out.println(server.dosa);
+        
+
+    }
+}
+
+
+
+
+
+
+
---
+++ forloop.java
@@ -0,0 +1,48 @@
+public class forloop {
+    public static void main(String args[]){
+
+    
+    //FOR LOOP EXERCISE SUGGESTED IN CHATGPT TO MASTER FOR LOOPS
+
+// // 1 to 10 number print
+//     for(int i=1;i<=10;i++){
+//         System.out.println(i);
+//     }
+
+// 1 to 20 even numbers
+
+// for(int i=4;i<=20;i+=2){
+    
+//     System.out.println(i);
+// }
+    
+//Sum of first 10 natural number
+
+int n = 20;
+int sum = 0;
+
+for(int i = 1;i<=n;i++){
+    sum += i;
+    System.out.println(sum);
+    
+}
+
+
+
+
+}
+
+
+
+    
+
+
+
+
+
+
+
+
+    }
+    
+
